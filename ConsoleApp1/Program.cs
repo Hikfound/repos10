a7c5a31b42058e50708dd00135f8fa22645b7d96
`Vlad` <ap5291433@gmail.com>
2023-03-23 12:01:15 +0200

    версия1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //метод пузырька
+            int[] array = { 25, 3, 5, 17, -9, 16, -23, -8, 12 };
+            for (int i=0; i<array.Length-1; i++)
+            {
+                for (int j=i+1;j<array.Length;j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int p = array[i];
+                        array[i] = array[j];
+                        array[j] = p;
+                    }
+                }
+            }
+            foreach (int elem in array)
+            {
+                Console.WriteLine($"{elem}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
